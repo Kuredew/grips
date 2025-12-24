914d02f666c5f244e64d688a6cd78cda18c5107c
Kuredew <burrohmanhabib35@gmail.com>
2025-12-24 21:35:07 +0700

    refactor: Inject logrus logger to struct

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,49 @@
 package cmd
 
 import (
+	"fmt"
 	"net/http"
+	"path"
+	"runtime"
+	"strings"
 
 	"github.com/Kuredew/grips/api/handler"
 	"github.com/Kuredew/grips/internal/downloader"
+	"github.com/sirupsen/logrus"
 )
 
-func Init() {
-	ytdlp := downloader.YtdlpDownloader{BinaryPath: "yt-dlp"}
+var log = logrus.New()
 
-	h := handler.DownloadHandler{Worker: ytdlp}
+func Init() {
+	// set logger formatter
+	log.SetFormatter(&logrus.TextFormatter{
+		TimestampFormat: "2006-01-02T15:04:05.000Z",
+		CallerPrettyfier: func(f *runtime.Frame) (string, string) {
+			// Extract just the function name
+			s := strings.Split(f.Function, ".")
+			funcname := s[len(s)-1]
+
+			_, filename := path.Split(f.File)
+
+			return fmt.Sprintf("[%v]", funcname), filename
+		},
+		FullTimestamp: true,
+		ForceColors:   true,
+	})
+	// set report caller
+	log.SetReportCaller(true)
+
+	ytdlp := downloader.YtdlpDownloader{
+		Log:        log,
+		BinaryPath: "yt-dlp",
+	}
+
+	h := handler.DownloadHandler{
+		Log:    log,
+		Worker: ytdlp,
+	}
+
+	log.Info("App Started.")
 
 	http.HandleFunc("/getinfo", h.HandleGetInfo)
 	http.HandleFunc("/extractvideo", h.HandleExtractVideo)
