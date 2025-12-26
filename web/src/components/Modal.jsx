e9fdbcf42214104e0edd4b3544d164f3b8cf8f49
Kuredew <burrohmanhabib35@gmail.com>
2025-12-26 22:27:57 +0700

    refactor: Change default width to dvw

--- web/src/components/Modal.jsx
+++ web/src/components/Modal.jsx
@@ -3,7 +3,7 @@ import BackIcon from "./icons/BackIcon";
 
 export default function Modal({title, close, children})  {
   return (
-    <div className="h-full w-full max-w-100 border-2 rounded-xl border-[#1F1F1F] bg-black">
+    <div className="h-full w-dvw max-w-100 border-2 rounded-xl border-[#1F1F1F] bg-black">
       <div id="header" className="flex justify-between p-5">
         <motion.button key={"back"} whileHover={{scale:1.1}} whileTap={{scale:0.95}} onClick={close} className="cursor-pointer scale-80">
           <BackIcon />
