a2dadf84025b67c61e7a78c18d01fff1cb8b0154
Kuredew <burrohmanhabib35@gmail.com>
2025-12-28 15:55:11 +0700

    fix: CORS problem with the dfferent localhost port

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,21 +1,33 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    headers: {
-          'Cross-Origin-Opener-Policy': 'same-origin',
-          'Cross-Origin-Embedder-Policy': 'require-corp',
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const BACKEND_URL = env.VITE_BACKEND_URL
+
+  return {
+    server: {
+      host: "0.0.0.0",
+      headers: {
+            'Cross-Origin-Opener-Policy': 'same-origin',
+            'Cross-Origin-Embedder-Policy': 'require-corp',
+          },
+      proxy: {
+        '/api': {
+          target: BACKEND_URL, 
+          changeOrigin: true, 
+          rewrite: (path) => path.replace(/^\/api/, ''), 
         },
-  },
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
-  optimizeDeps: {
-    exclude: ['@ffmpeg/ffmpeg']
+      },
+    },
+    plugins: [
+      tailwindcss(),
+      react()
+    ],
+    optimizeDeps: {
+      exclude: ['@ffmpeg/ffmpeg']
+    }
   }
 })
