d1cba3b89e0e670ff62d3937ec189d246268fce3
Kuredew <burrohmanhabib35@gmail.com>
2025-12-28 10:08:37 +0700

    refactor: Implement useSetting custom hook on component

--- web/src/pages/Settings/index.jsx
+++ web/src/pages/Settings/index.jsx
@@ -1,28 +1,11 @@
-import { useState } from "react";
 import SettingsNavigation from "./SettingsNavigation";
-import AppearanceSettings from "./AppearanceSettings";
-import { motion, AnimatePresence } from "motion/react";
-import VideoSettings from "./VideoSettings";
-import AudioSettings from "./AudioSettings";
-import Window from "../../components/Window";
 import SparklesIcon from "../../components/icons/SparklesIcon";
 import VideoIcon from "../../components/icons/VideoIcon";
 import AudioIcon from "../../components/icons/AudioIcon";
 import { useWindow } from "../../store/useWindow";
 
 export default function SettingsPage() {
-  const [activeMenu, setActiveMenu] = useState([])
   const { openWindow } = useWindow()
-
-  const closeMenu = (menu) => {
-    setActiveMenu(prev => {return prev.filter(active => active != menu)})
-  }
-
-  const addActiveMenu = (menu) => {
-    if (activeMenu.includes(menu)) return;
-
-    setActiveMenu(prev => [...prev, menu])
-  }
 
   const menus = [
     {
