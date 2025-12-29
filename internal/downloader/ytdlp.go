ea6874586b84bdb877115da60741cef88bf526cc
Kuredew <burrohmanhabib35@gmail.com>
2025-12-29 18:13:59 +0700

    feat: Add verbose and no playlist to display log more

--- internal/downloader/ytdlp.go
+++ internal/downloader/ytdlp.go
@@ -57,7 +57,7 @@ func (ytdlp *YtdlpDownloader) GetTitle(url string) (URLInfo, error) {
 func (ytdlp *YtdlpDownloader) Extract(options Options, logChan chan<- string) (URLInfo, error) {
 	var urlInfo URLInfo
 	var finalJsonString string
-	args := []string{"--print", `{"title": "%(title)s", "url": "%(urls)s" }`, "--cookies-from-browser", "firefox", "--js-runtimes", "node"}
+	args := []string{"--verbose", "--no-playlist", "--print", `{"title": "%(title)s", "url": "%(urls)s" }`, "--cookies-from-browser", "firefox", "--js-runtimes", "node"}
 
 	switch options.Mode {
 	case "video":
