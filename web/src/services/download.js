db19e10d60e2333953ae298f4e766cdf20215086
Kuredew <burrohmanhabib35@gmail.com>
2025-12-30 01:07:50 +0700

    refactor: Implement basic download function

--- web/src/services/download.js
+++ web/src/services/download.js
@@ -1,17 +1,27 @@
+import z from "zod";
 import { extractUrlInfo } from "./apiService";
+import { sleep } from "../utils/sleep";
+const API_URL = import.meta.env.VITE_API_URL
+
+const playlistDataListSchema = z.array(
+  z.object({
+    title: z.string(),
+    fileDataList: z.array(
+      z.instanceof(Uint8Array)
+    )
+  })
+)
 
 export const fetchFile = async (fileUrl, onProgress = () => {}) => {
-  console.log(`[${fetchFile.name}] fetching ${fileUrl}...`)
-  const progressObj = {
-    log: "",
-    progress: 0
-  }
-
   let chunksAll
   try {
-    const response = await fetch(fileUrl);
+    const requestUrl = `${API_URL}/proxy?url=${encodeURIComponent(fileUrl)}`
+    console.log(`[${fetchFile.name}] fetching ${requestUrl}...`)
+
+    const response = await fetch(requestUrl);
     const reader = response.body.getReader();
     const contentLength = +response.headers.get('Content-Length');
+    console.log(`[${fetchFile.name}] content length is ${contentLength}`)
 
     let receivedLength = 0;
     let chunks = []; 
@@ -24,10 +34,10 @@ export const fetchFile = async (fileUrl, onProgress = () => {}) => {
       receivedLength += value.length;
 
       const step = Math.round((receivedLength / contentLength) * 100);
+      console.log(`[${fetchFile.name}] progress: ${step}%`)
       onProgress({
-        ...progressObj,
         progress: step,
-        log: `downloading (${step})`
+        log: `downloading (${step}%)`
       })
     }
 
@@ -42,7 +52,7 @@ export const fetchFile = async (fileUrl, onProgress = () => {}) => {
     throw e
   }
 
-  onProgress({ ...progressObj, log: 'fetch completed.' })
+  onProgress({ log: 'fetch completed.' })
   console.log(`[${fetchFile.name}] fetch completed.`)
   return chunksAll;
 }
@@ -50,20 +60,46 @@ export const fetchFile = async (fileUrl, onProgress = () => {}) => {
 
 export const runDownloadTask = async (options, onProgress = () => {}) => {
   const progressObj = {
+    title: "",
     log: "",
     progress: 0
   }
 
-  onProgress({ ...progressObj, log: "getting ready..." })
+  onProgress({ log: "getting ready..." })
 
   try {
     console.log(`[${runDownloadTask.name}] requesting extract to api`)
 
-    const infoObj = await extractUrlInfo(options, (response) => {
-      onProgress({ ...progressObj, log: response.log })
+    const responseObj = await extractUrlInfo(options, (response) => {
+      onProgress({ log: response.log })
     })
 
-    console.log(`[${runDownloadTask.name}] got url info: ${JSON.stringify(infoObj, {}, 2)}`)
+    console.log(`[${runDownloadTask.name}] got url info: ${JSON.stringify(responseObj, {}, 2)}`)
+
+    const urlInfoList = responseObj.info
+    const playlistDataList = []
+
+    for (const urlInfo of urlInfoList) {
+      const urls = urlInfo.url.split(" | ")
+      const fileDataList = []
+
+      onProgress({ title: urlInfo.title, log: "cooldown 5 sec before download..."})
+      await sleep(5000)
+
+      for (const url of urls) {
+        const fileData = await fetchFile(url, onProgress)
+        fileDataList.push(fileData)
+      }
+
+      const newFile = {
+        title: urlInfo.title,
+        fileDataList: fileDataList
+      }
+      playlistDataList.push(newFile)
+    }
+
+    console.log(playlistDataList)
+    return playlistDataListSchema.parse(playlistDataList)
   } catch (e) {
     console.warn(`[${runDownloadTask.name}] error getting url info: ${e}`)
     onProgress({ ...progressObj, log: `${e}` })
